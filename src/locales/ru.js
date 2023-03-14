2c1012316ba8abc2441da8ab7388aa7eebcc0727
Kolotiy Pavel <kolotiy199420@mail.ru>
2023-03-15 01:17:49 +0300

    feat: add locales for i18next

---
+++ src/locales/ru.js
@@ -0,0 +1,15 @@
+export default {
+  translation: {
+    feedback: {
+      access: 'RSS успешно загружен',
+      errors: {
+        default: 'Данные неверные',
+        network: 'Ошибка сети',
+        doubleRss: 'RSS уже существует',
+        invalidUrl: 'Ссылка должна быть валидным URL',
+        invalidRss: 'Ресурс не содержит валидный RSS',
+        emptyInput: 'Не должно быть пустым',
+      },
+    },
+  },
+};
