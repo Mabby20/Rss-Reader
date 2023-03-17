be5b441d2c29a2a33671ba0aaf86ecdc2bc09d6a
Kolotiy Pavel <kolotiy199420@mail.ru>
2023-03-17 19:57:38 +0300

    feat(view.js): add buildPost function

--- src/js/view.js
+++ src/js/view.js
@@ -22,7 +22,46 @@ const buildFeed = (state) => {
   return card;
 };
 
-const handleLinks = (elements, i18n) => {
+const buildPost = (state) => {
+  const { postList } = state;
+
+  const card = document.createElement('div');
+  const cardBody = document.createElement('div');
+  const listGroup = document.createElement('ul');
+
+  card.classList.add('card', 'border-0');
+  card.append(cardBody, listGroup);
+  cardBody.outerHTML = '<div class="card-body"><h2 class="card-title h4">Посты</h2></div>';
+
+  const elements = postList.map((post) => {
+    const {
+      title, id, url, // description, feedId
+    } = post;
+
+    return `
+        <li class="list-group-item d-flex justify-content-between align-items-start border-0 border-end-0">
+            <a href=${url} class="fw-bold"
+                data-id=${id} target="_blank" rel="noopener noreferrer">${title}</a>
+            <button type="button" class="btn btn-outline-primary btn-sm" data-id=${id} data-bs-toggle="modal"
+                    data-bs-target="#modal">Просмотр</button>
+        </li>`;
+  }).join('\n');
+  listGroup.outerHTML = `<ul class="list-group border-0 rounded-0">${elements}</ul>`;
+  return card;
+};
+
+const outputContent = (elements, state) => {
+  elements.outputFeed.innerHTML = '';
+  elements.outputPost.innerHTML = '';
+
+  const feedList = buildFeed(state);
+  const postList = buildPost(state);
+
+  elements.outputFeed.append(feedList);
+  elements.outputPost.append(postList);
+};
+
+const outputSuccess = (elements, i18n) => {
   elements.input.classList.remove('is-invalid');
 
   elements.status.classList.remove('text-danger');
