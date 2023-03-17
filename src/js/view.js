89300d4b9e637bc9240a127a4131949249569f75
Kolotiy Pavel <kolotiy199420@mail.ru>
2023-03-17 19:53:14 +0300

    feat(view.js): add buildFeed function

--- src/js/view.js
+++ src/js/view.js
@@ -1,10 +1,25 @@
-const handleErrors = (elements, error, i18n) => {
-  elements.input.classList.add('is-invalid');
+const buildFeed = (state) => {
+  const { feedList } = state;
 
-  elements.status.classList.add('text-danger');
-  elements.status.classList.remove('text-success');
-  const [{ values }] = error.errors;
-  elements.status.textContent = i18n.t(values);
+  const card = document.createElement('div');
+  const cardBody = document.createElement('div');
+  const listGroup = document.createElement('ul');
+
+  card.classList.add('card', 'border-0');
+  card.append(cardBody, listGroup);
+  cardBody.outerHTML = '<div class="card-body"><h2 class="card-title h4">Фиды</h2></div>';
+
+  const elements = feedList.map((feed) => {
+    const { title, description } = feed;
+    return `
+        <li class="list-group-item border-0 border-end-0">
+            <h3 class="h6 m-0">${title}</h3
+            <p class="m-0 small text-black-50">${description}</p>
+        </li>`;
+  }).join('\n');
+
+  listGroup.outerHTML = `<ul class="list-group border-0 rounded-0">${elements}</ul>`;
+  return card;
 };
 
 const handleLinks = (elements, i18n) => {
