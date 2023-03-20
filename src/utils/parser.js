e12f58370faacd24323d43cb1b0656ed377bb468
Kolotiy Pavel <kolotiy199420@mail.ru>
2023-03-20 16:11:11 +0300

    fix: split the function
    main function of the parser is divided into 2 abstractions extractFeed and extractPosts

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -1,3 +1,37 @@
+const extractFeed = (rss) => {
+  const titleEl = rss.querySelector('channel > title');
+  const descriptionEl = rss.querySelector('channel > description');
+
+  if (!titleEl || !descriptionEl) {
+    throw new Error('Error parser');
+  }
+
+  const title = titleEl.textContent;
+  const description = descriptionEl.textContent;
+
+  return { title, description };
+};
+
+const extractPosts = (rss) => {
+  const items = rss.querySelectorAll('channel > item');
+
+  if (!items) {
+    throw new Error('Error parser');
+  }
+
+  return [...items].map((post) => {
+    const titleEl = post.querySelector('title');
+    const descriptionEl = post.querySelector('description');
+    const linkEl = post.querySelector('link');
+
+    const title = titleEl.textContent;
+    const description = descriptionEl.textContent;
+    const link = linkEl.textContent;
+
+    return { title, description, link };
+  });
+};
+
 export default (content) => {
   const parser = new DOMParser();
   const rss = parser.parseFromString(content, 'application/xml');
