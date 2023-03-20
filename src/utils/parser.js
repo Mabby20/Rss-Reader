b8cc92afe94c63eee5103b532e3945c7d358751f
Kolotiy Pavel <kolotiy199420@mail.ru>
2023-03-20 16:13:31 +0300

    fix: replace code with executing functions

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -34,35 +34,10 @@ const extractPosts = (rss) => {
 
 export default (content) => {
   const parser = new DOMParser();
-  const rss = parser.parseFromString(content, 'application/xml');
-  console.log('получившийся rss', rss);
-  const elements = {
-    title: rss.querySelector('title'),
-    description: rss.querySelector('description'),
-    link: rss.querySelector('link'),
-    posts: rss.querySelectorAll('item'),
-  };
-  const feed = {
-    title: elements.title.textContent,
-    description: elements.description.textContent,
-    url: elements.link.textContent,
-  };
+  const rss = parser.parseFromString(content, 'text/xml');
 
-  const posts = [...elements.posts].reduce((acc, item) => {
-    const postElements = {
-      title: item.querySelector('title'),
-      description: item.querySelector('description'),
-      url: item.querySelector('link'),
-    };
-
-    const post = {
-      title: postElements.title.textContent,
-      description: postElements.description.textContent,
-      url: postElements.url.textContent,
-    };
-
-    return [...acc, post];
-  }, []);
+  const feed = extractFeed(rss);
+  const posts = extractPosts(rss);
 
   return { feed, posts };
 };
