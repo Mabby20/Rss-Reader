d1868e1b3c2792b79703ac16ae068b6bcb180a8e
Kolotiy Pavel <kolotiy199420@mail.ru>
2023-03-21 02:51:51 +0300

    refactor: rename const

--- src/js/view.js
+++ src/js/view.js
@@ -25,17 +25,17 @@ const buildFeed = (state) => {
 const buildPost = (state) => {
   const { postList } = state.data;
 
-  const card = document.createElement('div');
-  const cardBody = document.createElement('div');
-  const listGroup = document.createElement('ul');
+  const cardEl = document.createElement('div');
+  const cardBodyEl = document.createElement('div');
+  const listGroupEl = document.createElement('ul');
 
-  card.classList.add('card', 'border-0');
-  card.append(cardBody, listGroup);
-  cardBody.outerHTML = '<div class="card-body"><h2 class="card-title h4">Посты</h2></div>';
+  cardEl.classList.add('card', 'border-0');
+  cardEl.append(cardBodyEl, listGroupEl);
+  cardBodyEl.outerHTML = '<div class="card-body"><h2 class="card-title h4">Посты</h2></div>';
 
   const elements = postList.map((post) => {
     const {
-      title, id, url, // description, feedId
+      title, id, link,
     } = post;
 
     const item = `
@@ -48,8 +48,8 @@ const buildPost = (state) => {
     return item;
   }).join('\n');
 
-  listGroup.outerHTML = `<ul class="list-group border-0 rounded-0">${elements}</ul>`;
-  return card;
+  listGroupEl.outerHTML = `<ul class="list-group border-0 rounded-0">${elements}</ul>`;
+  return cardEl;
 };
 
 const outputFeed = (elements, state) => {
