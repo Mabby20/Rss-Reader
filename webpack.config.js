b81209e0a751a75b0ff3af236e6b4d722f5851ad
Kolotiy Pavel <kolotiy199420@mail.ru>
2023-03-21 21:38:23 +0300

    build: remove babel from the project

--- webpack.config.js
+++ webpack.config.js
@@ -44,16 +44,6 @@ export default {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-      {
         test: /\.(sa|sc|c)ss$/,
         use: [
           (mode === "development") ? 'style-loader' : MiniCssExtractPlugin.loader,
